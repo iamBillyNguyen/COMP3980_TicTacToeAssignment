86ae3df918a83b9e5e03c02adf33bdbe1dc5b071
irislawly <irislawly@gmail.com>
2021-02-15 19:36:15 -0800

    client change

--- client.cpp
+++ client.cpp
@@ -25,226 +25,211 @@
 #include "tictac.h"
 #define PORT "8080"
 #define IPADD 3232235940
-#define SERV_HOST_ADDR "23.16.22.78"
 
+// #define SERV_HOST_ADDR "23.16.22.78"
+#define SERV_HOST_ADDR "127.0.0.1"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int sockfd, newsockfd, port_no, n, connectfd, bytes_sent, bytes_recvd;
-	char cbuffer[512], sname[64], cname[64];
-	char *ptr = &cbuffer[0];
-	char *ptr_port = (char *)&PORT;
-	struct sockaddr_in serv_addr;
-	struct hostent *he;
-	char x;
-	int count = 0, inp, y, ni, inp_true = 0, toss;
-	char serv_choice, cli_choice, nc;
-	char choice_buffer[2], co_ordinates_buffer[2], toss_buffer;
+    int sockfd, newsockfd, port_no, n, connectfd, bytes_sent, bytes_recvd;
+    char cbuffer[512], sname[64], cname[64];
+    char *ptr = &cbuffer[0];
+    char *ptr_port = (char *)&PORT;
+    struct sockaddr_in serv_addr;
+    struct hostent *he;
+    char x;
+    int count = 0, inp, y, ni, inp_true = 0, toss;
+    char serv_choice, cli_choice, nc;
+    char choice_buffer[2], co_ordinates_buffer[2], toss_buffer;
 
-	// system("clear");
+    // system("clear");
 
-	// if (argc != 2)
-	// {
-	// 	perror("Incomplete arguments!");
-	// 	return 1;
-	// }
-	port_no = atoi(ptr_port);
+    // if (argc != 2)
+    // {
+    // 	perror("Incomplete arguments!");
+    // 	return 1;
+    // }
+    port_no = atoi(ptr_port);
 
-	// if (he == NULL)
-	// {
-	// 	perror("No Such Host!");
-	// 	return 1;
-	// }
+    // if (he == NULL)
+    // {
+    // 	perror("No Such Host!");
+    // 	return 1;
+    // }
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1)
-	{
-		perror("Sorry. Socket could not be created!");
-		return 1;
-	}
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        perror("Sorry. Socket could not be created!");
+        return 1;
+    }
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(port_no);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port_no);
 
-	// serv_addr.sin_addr = *((struct in_addr *)he->h_addr);
-	serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
-	connectfd = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	if (connectfd == -1)
-	{
-		perror("Sorry. Could not connect to server.");
+    // serv_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    serv_addr.sin_addr.s_addr = inet_addr(SERV_HOST_ADDR);
+    connectfd = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (connectfd == -1)
+    {
+        perror("Sorry. Could not connect to server.");
 
-		//THIS THIING ENDS CLIENT!
-			//return 1;
-	}
+        //THIS THIING ENDS CLIENT!
+        //return 1;
+    }
 
-	//cout << "Enter your name : ";
-	// cin>>cname;
-	// do
-	// {
-	// 	static int flag = 0;
-	// 	bytes_sent = send(sockfd, &cname, sizeof(cname), 0);
-	// 	if (bytes_sent == -1 && flag == 0)
-	// 	{
-	// 		cout<<"PLAYER DATA NOT SENT!"<<endl<<"Trying Again...";
-	// 		continue;
-	// 	}
-	// 	else
-	// 	{		cli_choice = 'X';
+    //cout << "Enter your name : ";
+    // cin>>cname;
+    // do
+    // {
+    // 	static int flag = 0;
+    // 	bytes_sent = send(sockfd, &cname, sizeof(cname), 0);
+    // 	if (bytes_sent == -1 && flag == 0)
+    // 	{
+    // 		cout<<"PLAYER DATA NOT SENT!"<<endl<<"Trying Again...";
+    // 		continue;
+    // 	}
+    // 	else
+    // 	{		cli_choice = 'X';
 
-	// 		flag = 1;
-	// 		memset(&sname, 0, sizeof(sname));
-	// 		bytes_recvd = recv(sockfd, &sname, sizeof(sname), 0);
-	// 		if (bytes_recvd == -1)
-	// 			cout<<"COULD NOT ACQUIRE PLAYER INFORMATION!"<<endl<<"Trying Again..."<<endl;
-	// 		else
-	// 			cout<<"You have joined "<<sname<<" for a game of Tic-Tac-Toe."<<endl;
-	// 	}
-	// }while(bytes_sent == -1 || bytes_recvd == -1);
+    // 		flag = 1;
+    // 		memset(&sname, 0, sizeof(sname));
+    // 		bytes_recvd = recv(sockfd, &sname, sizeof(sname), 0);
+    // 		if (bytes_recvd == -1)
+    // 			cout<<"COULD NOT ACQUIRE PLAYER INFORMATION!"<<endl<<"Trying Again..."<<endl;
+    // 		else
+    // 			cout<<"You have joined "<<sname<<" for a game of Tic-Tac-Toe."<<endl;
+    // 	}
+    // }while(bytes_sent == -1 || bytes_recvd == -1);
 
-	 cout<<"Creating game. Please wait..."<<endl;
-	// sleep(2);
-	 cout<<endl<<"Game created!"<<endl<<endl<<"Doing a toss...";
+    cout << "Creating game. Please wait..." << endl;
+    // sleep(2);
+    cout << endl
+         << "Game created!" << endl
+         << endl
+         << "Doing a toss...";
 
-	// bytes_recvd = recv(sockfd, &toss_buffer, sizeof(toss_buffer), 0);
-	// if (bytes_recvd == -1)
-	// {
-	// 	perror("TOSS BUFFER not received");
-	// 	return 1;
-	// }
+     bytes_recvd = recv(sockfd, &toss_buffer, sizeof(toss_buffer), 0);
 
-	// toss = toss_buffer - '0';
-	// if (toss == 0)
-	// {
-	// 	cout<<endl<<sname<<" WON the toss."<<endl;
-	// 	cout<<sname<<" is choosing. Please wait..."<<endl<<endl;
-	// 	memset(&choice_buffer, 0, sizeof(choice_buffer));
-	// 	bytes_recvd = recv(sockfd, &choice_buffer, sizeof(choice_buffer), 0);
-	// 	if (bytes_recvd == -1)
-	// 	{
-	// 		perror("CHOICE BUFFER not received!");
-	// 		return 1;
-	// 	}
-	// 	else
-	// 	{
-	// 		serv_choice = choice_buffer[0];
-	// 		cli_choice = choice_buffer[1];
-	// 		cout<<sname<<" has chosen "<<serv_choice<<endl<<endl<<"You will play with "<<cli_choice<<endl;
-	// 		cout<<endl<<"Lets Play!"<<endl<<endl;
-	// 	}
-	// }
-	// else
-	// {
-	// 	cout<<endl<<"You won the TOSS!"<<endl;
-	// 	do
-	// 	{
-	// 		cout<<cname<<" Enter Your Choice (X or O): ";
-	// 		cin>>cli_choice;
-	// 		if (cli_choice == 'X' || cli_choice == 'x')
-	// 		{
-	//     		serv_choice = 'O';
-	//     		cli_choice = 'X';
-	//     		inp_true = 1;
-	// 			cout<<endl<<sname<<" gets O."<<endl<<endl<<"Lets Play!"<<endl<<endl;
-	// 		}
-	// 		else if (cli_choice == 'O' || cli_choice == 'o' || cli_choice == '0')
-	// 		{
-	// 		    serv_choice = 'X';
-	//     		cli_choice = 'O';
-	//     		inp_true = 1;
-	//     		cout<<endl<<sname<<" gets X."<<endl<<endl<<"Lets Play!"<<endl<<endl;
-	// 		}
-	// 		else
-	// 		{
-	//     		cout<<"\nInvalid Choice! Please Choose Again..."<<endl;
-	//     		inp_true == 0;
-	// 		}
-	// 	}while(inp_true == 0);
+     if (bytes_recvd == -1)
+     {
+     	perror("TOSS BUFFER not received");
+     	return 1;
+     }
 
-	// 	memset(&choice_buffer, 0, sizeof(choice_buffer));
-	// 	choice_buffer[0] = serv_choice;
-	// 	choice_buffer[1] = cli_choice;
+     toss = toss_buffer - '0';
+     cout << "toss: " << toss << endl;
+    // if (toss == 0)
+    // {
+    // 	cout<<endl<<sname<<" WON the toss."<<endl;
+    // 	cout<<sname<<" is choosing. Please wait..."<<endl<<endl;
+    // 	memset(&choice_buffer, 0, sizeof(choice_buffer));
+    // 	bytes_recvd = recv(sockfd, &choice_buffer, sizeof(choice_buffer), 0);
+    // 	if (bytes_recvd == -1)
+    // 	{
+    // 		perror("CHOICE BUFFER not received!");
+    // 		return 1;
+    // 	}
+    // 	else
+    // 	{
+    // 		serv_choice = choice_buffer[0];
+    // 		cli_choice = choice_buffer[1];
+    // 		cout<<sname<<" has chosen "<<serv_choice<<endl<<endl<<"You will play with "<<cli_choice<<endl;
+    // 		cout<<endl<<"Lets Play!"<<endl<<endl;
+    // 	}
+    // }
+    // else
+    // {
+    // 	cout<<endl<<"You won the TOSS!"<<endl;
+    // 	do
+    // 	{
+    // 		cout<<cname<<" Enter Your Choice (X or O): ";
+    // 		cin>>cli_choice;
+    // 		if (cli_choice == 'X' || cli_choice == 'x')
+    // 		{
+    //     		serv_choice = 'O';
+    //     		cli_choice = 'X';
+    //     		inp_true = 1;
+    // 			cout<<endl<<sname<<" gets O."<<endl<<endl<<"Lets Play!"<<endl<<endl;
+    // 		}
+    // 		else if (cli_choice == 'O' || cli_choice == 'o' || cli_choice == '0')
+    // 		{
+    // 		    serv_choice = 'X';
+    //     		cli_choice = 'O';
+    //     		inp_true = 1;
+    //     		cout<<endl<<sname<<" gets X."<<endl<<endl<<"Lets Play!"<<endl<<endl;
+    // 		}
+    // 		else
+    // 		{
+    //     		cout<<"\nInvalid Choice! Please Choose Again..."<<endl;
+    //     		inp_true == 0;
+    // 		}
+    // 	}while(inp_true == 0);
 
-	// 	bytes_sent = send(sockfd, &choice_buffer, sizeof(choice_buffer), 0);
-	// 	if (bytes_sent == -1)
-	// 	{
-	// 		perror("CHOICE BUFFER could not be sent.");
-	// 		return 1;
-	// 	}
+    // 	memset(&choice_buffer, 0, sizeof(choice_buffer));
+    // 	choice_buffer[0] = serv_choice;
+    // 	choice_buffer[1] = cli_choice;
 
-	// }
+    // 	bytes_sent = send(sockfd, &choice_buffer, sizeof(choice_buffer), 0);
+    // 	if (bytes_sent == -1)
+    // 	{
+    // 		perror("CHOICE BUFFER could not be sent.");
+    // 		return 1;
+    // 	}
 
-	// if (serv_choice == 'X')
-	// {
-	// 	inp = 1;
-	// 	cout<<sname<<" will play first."<<endl<<endl;
+    // }
 
-	// }
-	// else
-	// {
-	// 	inp = 2;
-	// 	cout<<"You will play first."<<endl<<endl;
-	// }
+    // if (serv_choice == 'X')
+    // {
+    // 	inp = 1;
+    // 	cout<<sname<<" will play first."<<endl<<endl;
 
-	// init();
-	cout << endl
-		 << "Starting Game..." << endl;
-	// sleep(3);
-	display();
+    // }
+    // else
+    // {
+    // 	inp = 2;
+    // 	cout<<"You will play first."<<endl<<endl;
+    // }
 
-	while (count < 9)
-	{
-		memset(&co_ordinates_buffer, 0, sizeof(co_ordinates_buffer));
+    // init();
+    cout << endl
+         << "Starting Game..." << endl;
+    // sleep(3);
+    display();
 
-			cout << endl
-				 << "Your turn. Enter co-ordinates separated by a space : ";
-			cin >> x;
-			ni = input(x);
-			if (ni == 0)
-			{
-				inp++;
+    while (count < 9)
+    {
+        memset(&co_ordinates_buffer, 0, sizeof(co_ordinates_buffer));
 
-				cout << "sent " << x << " to server!" << endl;
- 				bytes_sent = send(sockfd, &co_ordinates_buffer, sizeof(co_ordinates_buffer), 0);
-				if (bytes_sent == -1)
-				{
-					perror("CO-ORDINATES BUFFER could not be sent!");
-					return 1;
-				}
-			}
-		
+        cout << endl
+             << "Your turn. Enter co-ordinates separated by a space : ";
+        cin >> x;
+        ni = input(x);
+        cout << "\n" << ni << "\n";
+        if (ni == 0)
+        {
+            inp++;
 
-		// 	count ++;
-		// 	sleep(2);
-		// 	system("clear");
-		// 	display();
+            cout << "sent " << x << " to server!" << endl;
+            bytes_sent = send(sockfd, &co_ordinates_buffer, sizeof(co_ordinates_buffer), 0);
 
-		// 	if (count >=5)
-		// 	{
-		// 		nc = check();
-		// 		if (nc == 'f')
-		// 			continue;
-		// 		else if (serv_choice == nc)
-		// 		{
-		// 			cout<<endl<<"You loose."<<endl<<sname<<" has won."<<endl;
-		// 			break;
-		// 		}
-		// 		else if (cli_choice == nc)
-		// 		{
-		// 			cout<<endl<<"Congrats! You have won!!!"<<endl<<sname<<" lost."<<endl;
-		// 			break;
-		// 		}
-		// 	}
+            if (bytes_sent == -1)
+            {
+                perror("CO-ORDINATES BUFFER could not be sent!");
+                return 1;
+            }
+        }
 
-		// }
+        count++;
+    }
 
-		// if (nc == 'f')
-		// 	cout<<endl<<"Game ends in a draw."<<endl;
+    // if (nc == 'f')
+    // 	cout<<endl<<"Game ends in a draw."<<endl;
 
-		cout << endl
-			 << "Thank You for playing Tic-tac-Toe" << endl;
-		close(sockfd);
-		return 0;
-	}
+    cout << endl
+         << "Thank You for playing Tic-tac-Toe" << endl;
+    close(sockfd);
+    return 0;
 }
