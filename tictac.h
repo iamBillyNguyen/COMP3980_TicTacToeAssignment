5f943b50167de848a1af566021be3445633b8e19
irislawly <irislawly@gmail.com>
2021-02-16 01:46:04 -0800

    commenting

--- tictac.h
+++ tictac.h
@@ -10,20 +10,15 @@ void display();
 int input(char, int, int);
 char check();
 
-void init()
-{
-    int i, j;
-    for (i=0; i<3; i++)
-		for (j=0; j<3; j++)
-            mat[i][j] = '-';
-}    
-
+/**
+ * Function to display the formatting of the tic tac toe game.
+ */
 void display()
 {
 	
 	int i, j;
 	system("clear");
-	cout<<"\nWelcome to the Tic-Tac-Toe Game!\n\nGrid Co-ordinates : "<<endl<<endl;
+	cout<<"\nWelcome to the Tic-Tac-Toe Game!\n\n Spots"<<endl<<endl;
     char letter = 'A';
     for (i=0; i<3; i++)
 	{	
@@ -36,16 +31,12 @@ void display()
 	
 	}       
 
-	cout<<"\nThe Tic-Tac-Toe board : "<<endl<<endl;
-	for (i=0; i<3; i++)
-	{
-		cout<<"\t";
-	    for (j=0; j<3; j++)
-		    cout<<" "<<mat[i][j]<<"     ";
-		cout<<endl;
-	}		
 }
-
+/**
+ * Input validation.
+ * @param choice player choice
+ * @return 0 if valid, 1 if false
+ */
 int input(char choice[])
 {
 	char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
@@ -60,69 +51,12 @@ int input(char choice[])
 	return -1;
 }
 
-char check()
-{
-	int i, j;
-	char temp;
-
-	for (j=0; j<3; j++)
-	{
-		if (mat[0][j] != '-')
-		{
-			temp = mat[0][j];
-			if (j == 0)
-			{
-				if (mat[0][1] == temp)
-					if (mat[0][2] == temp)
-						return temp;
-				if (mat[1][0] == temp)
-					if (mat[2][0] == temp)
-						return temp;
-				if (mat[1][1] == temp)
-					if (mat[2][2] == temp)	
-						return temp;				
-			}
-			else if (j == 1)	
-			{
-				if (mat[1][1] == temp)
-					if (mat[2][1] == temp)
-						return temp;
-			}
-			else 
-			{
-				if (mat[1][2] == temp)
-					if (mat[2][2] == temp)
-						return temp;
-			}
-		}
-	}
-	for (i=1; i<3; i++)
-	{
-		if (mat[i][0] != '-')
-		{
-			temp = mat[i][0];
-			if (i == 1)
-			{
-				if (mat[1][1] == temp)
-					if (mat[1][2] == temp)
-						return temp;
-			}
-			else
-			{
-				if (mat[2][1] == temp)
-					if (mat[2][2] == temp)
-						return temp;
-				if (mat[1][1] == temp)
-					if (mat[0][2] == temp)
-						return temp;
-			}
-		}
-	}
-	
-	return 'f';
-
-}
-
+/**
+ * Function to display board.
+ * @param c letter
+ * @param board board format
+ * @param player player
+ */
 void update_board(char c, char board[][3], char player) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
